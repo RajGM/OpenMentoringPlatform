9c22ab17f7751438866746dece0fb2f4c3371863
Raj Gaurav Maurya <rajgmsocial19@gmail.com>
2023-09-23 02:18:27 +0530

    feat: enter image uploader converted to tsx

--- components/ImageUploader.tsx
+++ components/ImageUploader.tsx
@@ -3,19 +3,19 @@ import { auth, storage, STATE_CHANGED } from '@lib/firebase';
 import Loader from './Loader';
 
 // Uploads images to Firebase Storage
-export default function ImageUploader() {
-  const [uploading, setUploading] = useState(false);
-  const [progress, setProgress] = useState(0);
-  const [downloadURL, setDownloadURL] = useState(null);
+const ImageUploader: React.FC = () => {
+  const [uploading, setUploading] = useState<boolean>(false);
+  const [progress, setProgress] = useState<number>(0);
+  const [downloadURL, setDownloadURL] = useState<string | null>(null);
 
   // Creates a Firebase Upload Task
-  const uploadFile = async (e) => {
+  const uploadFile = async (e: React.ChangeEvent<HTMLInputElement>) => {
     // Get the file
-    const file = Array.from(e.target.files)[0];
+    const file = Array.from(e.target.files!)[0]; // Note the use of "!" to assert that files is non-null
     const extension = file.type.split('/')[1];
 
     // Makes reference to the storage bucket location
-    const ref = storage.ref(`uploads/${auth.currentUser.uid}/${Date.now()}.${extension}`);
+    const ref = storage.ref(`uploads/${auth.currentUser!.uid}/${Date.now()}.${extension}`); // Note the use of "!" to assert that currentUser is non-null
     setUploading(true);
 
     // Starts the upload
@@ -24,7 +24,7 @@ export default function ImageUploader() {
     // Listen to updates to upload task
     task.on(STATE_CHANGED, (snapshot) => {
       const pct = ((snapshot.bytesTransferred / snapshot.totalBytes) * 100).toFixed(0);
-      setProgress(pct);
+      setProgress(Number(pct));
     });
 
     // Get downloadURL AFTER task resolves (Note: this is not a native Promise)
@@ -54,3 +54,5 @@ export default function ImageUploader() {
     </div>
   );
 }
+
+export default ImageUploader;
--- pages/enter/index.tsx
+++ pages/enter/index.tsx
@@ -10,12 +10,9 @@ import Loader from "@components/Loader";
 
 import Link from "next/link";
 
-export default function Enter(props) {
+export default function Enter(props: any) {
   const { user, username } = useContext(UserContext);
 
-  // 1. user signed out <SignInButton />
-  // 2. user signed in, but missing username <UsernameForm />
-  // 3. user signed in, has username <SignOutButton />
   return (
     <main className="middle fullHeight">
       <div>
@@ -25,7 +22,6 @@ export default function Enter(props) {
   );
 }
 
-// Sign in with Google button
 function SignInButton() {
   const signInWithGoogle = async () => {
     await auth.signInWithPopup(googleAuthProvider);
@@ -48,7 +44,6 @@ function SignInButton() {
   );
 }
 
-// Sign out button
 function HomePage() {
   return (
     <Link href="/">
@@ -57,39 +52,34 @@ function HomePage() {
   );
 }
 
-// Username form
 function UsernameForm() {
-  const [formValue, setFormValue] = useState("");
-  const [isValid, setIsValid] = useState(false);
-  const [loading, setLoading] = useState(false);
+  const [formValue, setFormValue] = useState<string>("");
+  const [isValid, setIsValid] = useState<boolean>(false);
+  const [loading, setLoading] = useState<boolean>(false);
 
   const { user, username } = useContext(UserContext);
 
-  const onSubmit = async (e) => {
+  const onSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
 
-    // Create refs for both documents
-    const userDoc = firestore.doc(`users/${user.uid}`);
+    const userDoc = firestore.doc(`users/${user!.uid}`);
     const usernameDoc = firestore.doc(`usernames/${formValue}`);
 
-    // Commit both docs together as a batch write.
     const batch = firestore.batch();
     batch.set(userDoc, {
       username: formValue,
-      photoURL: user.photoURL,
-      displayName: user.displayName,
+      photoURL: user!.photoURL,
+      displayName: user!.displayName,
     });
-    batch.set(usernameDoc, { uid: user.uid });
+    batch.set(usernameDoc, { uid: user!.uid });
 
     await batch.commit();
   };
 
-  const onChange = (e) => {
-    // Force form value typed in form to match correct format
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const val = e.target.value.toLowerCase();
     const re = /^(?=[a-zA-Z0-9._]{3,15}$)(?!.*[_.]{2})[^_.].*[^_.]$/;
 
-    // Only set form value if length is < 3 OR it passes regex
     if (val.length < 3) {
       setFormValue(val);
       setLoading(false);
@@ -103,20 +93,15 @@ function UsernameForm() {
     }
   };
 
-  //
-
   useEffect(() => {
     checkUsername(formValue);
   }, [formValue]);
 
-  // Hit the database for username match after each debounced change
-  // useCallback is required for debounce to work
   const checkUsername = useCallback(
-    debounce(async (username) => {
+    debounce(async (username: string) => {
       if (username.length >= 3) {
         const ref = firestore.doc(`usernames/${username}`);
         const { exists } = await ref.get();
-        console.log("Firestore read executed!");
         setIsValid(!exists);
         setLoading(false);
       }
@@ -158,7 +143,13 @@ function UsernameForm() {
   );
 }
 
-function UsernameMessage({ username, isValid, loading }) {
+interface UsernameMessageProps {
+  username: string;
+  isValid: boolean;
+  loading: boolean;
+}
+
+function UsernameMessage({ username, isValid, loading }: UsernameMessageProps) {
   if (loading) {
     return <p>Checking...</p>;
   } else if (isValid) {
